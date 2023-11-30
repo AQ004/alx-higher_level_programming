93b1f509e73a95be194f63f0a1abe3751cfd18f6
abdulla <amgaamams@gmail.com>
2023-11-30 15:12:11 -0800

    Completed

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    lenOfArgs = len(sys.argv) - 1
+    if lenOfArgs == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(lenOfArgs))
+        for args in range (1, lenOfArgs + 1):
+            print("{}: {}".format(args, sys.argv[args]))
